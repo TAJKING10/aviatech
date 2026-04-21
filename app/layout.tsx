9615be0afe4692c426c6c90ded000ccbbe92a273
TOUFIC-JANDAH <toufic-jandah@hotmail.com>
2026-04-21 15:20:49 +0300

    fix favicon - add direct link tags and favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+        <link rel="shortcut icon" type="image/png" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
